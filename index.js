c833ab49ee1eaa4ebbd89c0a713ccc73e1459b35
Jacob Harris <jacobwho07@gmail.com>
2026-06-09 12:13:05 -0500

    aded js solution to assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const code1 = 7 + 3;
+const code2 = (100 - 20) / 2;
+const code3 = 39 % 1 + 39;
+
+const message = "The vault has been secured. The combination is "
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`
+
+console.log(`${message}${codeA}, ${codeB}`);
